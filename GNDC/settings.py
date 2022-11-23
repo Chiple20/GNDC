b744de08988cea37fbe5c450b16d89c89cadb582
Angel Chiple <angel.chiple20@gmail.com>
2022-11-23 16:51:59 -0300

    cambios10

--- GNDC/settings.py
+++ GNDC/settings.py
@@ -141,8 +141,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 #STATIC_URL = '/static/'
-STATIC_URL = os.environ.get("DJANGO_STATIC_URL", "/static/")
-STATIC_ROOT = os.environ.get("DJANGO_STATIC_ROOT", "./static/")    
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+STATIC_URL = '/static/'  
 # para los ccs y todo lo demas 
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'GNDC/static'),)
 
